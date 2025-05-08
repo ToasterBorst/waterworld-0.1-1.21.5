e18a9d216b1fd0859aba6409a525d6dfd149ffb1
ToasterBorst <justindavidaaron@gmail.com>
2025-05-08 14:22:28 -0700

    .

--- src/main/kotlin/pww/modid/Waterworld.kt
+++ src/main/kotlin/pww/modid/Waterworld.kt
@@ -5,17 +5,11 @@ import org.slf4j.LoggerFactory
 
 object Waterworld : ModInitializer {
     private val logger = LoggerFactory.getLogger("waterworld")
-    
-    // Define constants
-    const val WATER_LEVEL = 126
-    const val MOD_ID = "waterworld"
 
     override fun onInitialize() {
-        logger.info("Project Waterworld is initializing!")
-        logger.info("Setting sea level to $WATER_LEVEL")
-        logger.info("Preventing terrain generation above ${WATER_LEVEL - 15}")
-        
-        // We'll use mixins for the actual modifications
-        logger.info("Waterworld terrain modification active")
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+        logger.info("Hello Fabric world!")
     }
 }
