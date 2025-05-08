5552e8250f9571cfc92f7e45e980a05b459153d7
ToasterBorst <justindavidaaron@gmail.com>
2025-05-08 12:24:45 -0700

    Initial commit

---
+++ src/client/kotlin/pww/modid/WaterworldClient.kt
@@ -0,0 +1,9 @@
+package pww.modid
+
+import net.fabricmc.api.ClientModInitializer
+
+object WaterworldClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/kotlin/pww/modid/WaterworldDataGenerator.kt
@@ -0,0 +1,9 @@
+package pww.modid
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object WaterworldDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
