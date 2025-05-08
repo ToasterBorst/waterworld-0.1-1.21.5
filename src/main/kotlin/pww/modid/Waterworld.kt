3033605ffc849af57050883d8270dce654edeccd
ToasterBorst <justindavidaaron@gmail.com>
2025-05-08 14:40:12 -0700

    .

--- src/main/kotlin/pww/modid/Waterworld.kt
+++ src/main/kotlin/pww/modid/Waterworld.kt
@@ -6,10 +6,10 @@ import org.slf4j.LoggerFactory
 object Waterworld : ModInitializer {
     private val logger = LoggerFactory.getLogger("waterworld")
 
-    override fun onInitialize() {
-        // This code runs as soon as Minecraft is in a mod-load-ready state.
-        // However, some things (like resources) may still be uninitialized.
-        // Proceed with mild caution.
-        logger.info("Hello Fabric world!")
-    }
+	override fun onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		logger.info("Hello Fabric world!")
+	}
 }
