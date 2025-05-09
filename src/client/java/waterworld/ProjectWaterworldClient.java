be68d1a475e1293c91e86f27ec8e8dd01a2e24da
ToasterBorst <justindavidaaron@gmail.com>
2025-05-09 13:27:24 -0700

    Fresh.

---
+++ src/client/java/waterworld/ProjectWaterworldClient.java
@@ -0,0 +1,10 @@
+package waterworld;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ProjectWaterworldClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
