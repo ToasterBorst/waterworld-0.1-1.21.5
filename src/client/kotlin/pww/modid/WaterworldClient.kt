555ca2b2c3810e0d58a3c09a4a3e85e1b63bc232
ToasterBorst <justindavidaaron@gmail.com>
2025-05-09 09:57:40 -0700

    .

--- src/client/kotlin/pww/modid/WaterworldClient.kt
+++
@@ -1,9 +0,0 @@
-package pww.modid
-
-import net.fabricmc.api.ClientModInitializer
-
-object WaterworldClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
-}
