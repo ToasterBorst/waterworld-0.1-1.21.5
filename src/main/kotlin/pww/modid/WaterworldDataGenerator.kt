05078631f30b7280e33d72e1b2863ca4e77ce2dd
ToasterBorst <justindavidaaron@gmail.com>
2025-05-09 13:26:10 -0700

    Wipe.

--- src/main/kotlin/pww/modid/WaterworldDataGenerator.kt
+++
@@ -1,9 +0,0 @@
-package pww.modid
-
-import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
-import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
-
-object WaterworldDataGenerator : DataGeneratorEntrypoint {
-	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
-	}
-}
